ff0e19505987314d416afa169262fd9b2327aa4a
David <david.lanyi101@gmail.com>
2021-02-27 14:09:15 -0500

    I think Timepoints are good

--- Hackathon Joint Calendar/TimePoint.cpp
+++ Hackathon Joint Calendar/TimePoint.cpp
@@ -1,24 +1,141 @@
 #include "TimePoint.h"
 
+void TimePoint::PrintTimePoint()
+{
+    m_time.PrintTime();
+    m_date.PrintDate();
+}
+
 TimePoint TimePoint::operator-(TimePoint& leftHandSide)
 {
     return TimePoint();
 }
 
-bool TimePoint::Date::ValidateDate(uint32_t day, Month month)
+void TimePoint::Date::PrintMonth()
 {
-    return false;
+    typedef TimePoint::Date::Month Month;
+    switch (month) {
+    case Month::January:
+            COUT("January ");
+            break;
+    case Month::February:
+        COUT("February ");
+        break;
+    case Month::March:
+        COUT("March ");
+        break;
+    case Month::April:
+        COUT("April ");
+        break;
+    case Month::June:
+        COUT("June ");
+        break;
+    case Month::July:
+        COUT("July ");
+        break;
+    case Month::August:
+        COUT("August ");
+        break;
+    case Month::September:
+        COUT("September ");
+        break;
+    case Month::October:
+        COUT("October ");
+        break;
+    case Month::November:
+        COUT("November ");
+        break;
+    case Month::December:
+        COUT("December ");
+        break;
+
+    }
+}
+
+bool TimePoint::Date::ValidateDate(uint32_t day, Month month, uint32_t year)
+{
+    typedef TimePoint::Date::Month Month;
+
+    if (year < 2000 || year>2200)
+        return false;
+
+    bool returnBool = false;
+
+    switch (month){
+    case Month::January:
+        if (day <= 31)
+            returnBool = true;
+        break;
+    case Month::February:
+        bool isLeapYear = (year % 4 == 0 && year % 100 != 0);
+        if (isLeapYear) {
+            if(day<=29)
+                returnBool = true;
+        }
+        else {
+            if (day <= 28)
+                returnBool = true;
+        }
+        break;
+    case Month::March:
+        if (day <= 31)
+            returnBool = true;
+        break;
+    case Month::April:
+        if (day <= 30)
+            returnBool = true;
+        break;
+    case Month::May:
+        if (day <= 31)
+            returnBool = true;
+        break;
+    case Month::June:
+        if (day <= 30)
+            returnBool = true;
+        break;
+    case Month::July:
+        if (day <= 31)
+            returnBool = true;
+        break;
+    case Month::August:
+            if (day <= 31)
+                returnBool = true;
+            break;
+    case Month::September:
+        if (day <= 30)
+            returnBool = true;
+        break;
+    case Month::October:
+        if (day <= 31)
+            returnBool = true;
+        break;
+    case Month::November:
+        if (day <= 30)
+            returnBool = true;
+        break;
+    case Month::December:
+        if (day <= 31)
+            returnBool = true;
+        break;
+            
+    }
+    return returnBool;
 }
 
 
-
-TimePoint::Date TimePoint::Date::CreateDate(uint32_t day, Month month)
+TimePoint::Date TimePoint::Date::CreateDate(uint32_t day, Month month, uint32_t year)
 {
-    if (ValidateDate(day, month))
-        return TimePoint::Date(day, month);
+    if (ValidateDate(day, month, year))
+        return TimePoint::Date(day, month, year);
     else
-        return TimePoint::Date(0, TimePoint::Date::Month::Invalid);
+        return TimePoint::Date(0, TimePoint::Date::Month::Invalid, 0);
    
+}
+
+void TimePoint::Date::PrintDate()
+{
+    PrintMonth();
+    COUT(" " << day << ", " << year);
 }
 
 bool TimePoint::Time::ValidateTime(uint32_t minute, uint32_t hour)
@@ -34,5 +151,10 @@ TimePoint::Time TimePoint::Time::CreateTime(uint32_t minute, uint32_t hour)
     if (ValidateTime(minute, hour))
         return Time(minute, hour);
     else
-        return Time(-1, -1);
+        return Time(61, 25);
+}
+
+void TimePoint::Time::PrintTime()
+{
+    COUT(hour << ":" << minute << ", ");
 }
--- Hackathon Joint Calendar/TimePoint.h
+++ Hackathon Joint Calendar/TimePoint.h
@@ -2,15 +2,17 @@
 #include "Common.h"
 class TimePoint
 {
+public:
+	void PrintTimePoint();
 private:
 	struct Date {
 
 	public:
-
+		
 		enum class Month {
 			Invalid = 0,
 			January = 1,
-			Febuary = 2,
+			February = 2,
 			March = 3,
 			April = 4,
 			May = 5,
@@ -23,19 +25,26 @@ private:
 			December = 12
 		};
 
+	public:
+
+		void PrintMonth();
+
 	private:
 
-		Date(uint32_t day, Month month) {
+		Date(uint32_t day, Month month, uint32_t year) {
 			this->day = day;
 			this->month = month;
+			this->year = year;
 		}
-		bool ValidateDate(uint32_t day, Month month);
+		bool ValidateDate(uint32_t day, Month month, uint32_t year);
 
 	public:
 
-		Date CreateDate(uint32_t day, Month month);
+		Date CreateDate(uint32_t day, Month month, uint32_t m_year);
+		void PrintDate();
 		uint32_t day;
 		Month month;
+		uint32_t year;
 	};
 	struct Time {
 	private:
@@ -47,6 +56,7 @@ private:
 		bool ValidateTime(uint32_t minute, uint32_t hour);
 	public:
 		Time CreateTime(uint32_t minute, uint32_t hour);
+		void PrintTime();
 		uint32_t minute;
 		uint32_t hour;
 	};
@@ -57,6 +67,7 @@ public:
 private:
 	Date m_date;
 	Time m_time;
+	
 
 };
 
